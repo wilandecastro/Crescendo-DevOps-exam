e7b57ca85dbe1b054692286e88f8a100a7ad6a83
Wilan De Castro <65393800+wilandecastro@users.noreply.github.com>
2024-09-18 21:47:28 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,25 +13,26 @@ resource "aws_vpc" "main_vpc" {
 }
 
 # Create Public Subnets
-resource "aws_subnet" "public_1" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"  # Adjust this to your desired AZ
+
   tags = {
-    Name = "Public-Subnet-1"
+    Name = "Public Subnet 1"
   }
 }
 
-resource "aws_subnet" "public_2" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"  # Adjust this to your desired AZ
+
   tags = {
-    Name = "Public-Subnet-2"
+    Name = "Public Subnet 2"
   }
 }
+
 
 # Create Private Subnets
 resource "aws_subnet" "private_1" {
@@ -72,15 +73,38 @@ resource "aws_nat_gateway" "nat_gateway" {
     Name = "Crescendo-NAT"
   }
 }
-
-# Create Route Tables for Subnets
-resource "aws_route_table" "public_route_table" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+
+  tags = {
+    Name = "Main IGW"
   }
 }
+
+# Create Route Tables for Subnets
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 
 #security group for your Application Load Balancer
 resource "aws_route_table_association" "public_assoc_1" {
@@ -120,6 +144,7 @@ resource "aws_lb" "app_lb" {
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
   }
+
 
 # EC2 Instance with Nginx and Tomcat Installed
 resource "aws_instance" "app_server" {
