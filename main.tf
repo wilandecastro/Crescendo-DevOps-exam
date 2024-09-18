e34b586831fe935eed9bf97412ab9930805a3aad
Wilan De Castro <65393800+wilandecastro@users.noreply.github.com>
2024-09-18 21:33:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
