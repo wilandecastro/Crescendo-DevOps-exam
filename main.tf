0c019221c54fbc9f04dd5bc39998490ab387acae
Wilan De Castro <65393800+wilandecastro@users.noreply.github.com>
2024-09-18 21:14:19 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "aws_lb" "app_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
-
+  }
 # EC2 Instance with Nginx and Tomcat Installed
 resource "aws_instance" "app_server" {
   ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI
